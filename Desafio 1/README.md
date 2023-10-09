f07d4f1b58adf6769cc0ee155563ceddb9f0f380
giltemarSantos <giltemarsantos@gmail.com>
2023-10-09 16:18:30 -0300

    Desafio do dia 2 - Alimentar os HTML usando JS

---
+++ Desafio 1/README.md
@@ -0,0 +1,21 @@
+# Desafio 1 -  Enviar texto para o HTML de forma dinâmica via JS:notebook:
+
+
+
+##### Objetivo:writing_hand:
+
+_Enviar de forma dinâmica através do JS os dados de um comprovante de registro de ponto_
+
+_Este exercício tem como objetivo a fixação dos conteúdos do dia_2 que foram apresentados até o presente momento._
+
+_Todas as imagens pertencem a empresa **[Solides Tangerino](https://tangerino.com.br/)**_
+
+
+
+##### Note:writing_hand:
+
+> HTML
+>
+> CSS
+>
+> JavaScript
---
+++ Desafio 1/index.js
@@ -0,0 +1,26 @@
+//name colaborador
+document.getElementById('nameColaborador').innerHTML = 'alexander pires almeida'
+
+//name Empresa
+document.getElementById('coorperative').innerHTML = 'Gct - Gerenciamento e Controle de Transito S/A'
+
+//name CNPJ
+document.getElementById('CNPJ').innerHTML = '01.466.431/0001-00'
+
+//name Endereço
+document.getElementById('endereco').innerHTML = 'Rodovia Jorge Amado SN - Banco da Vitória - Ilhéus - Bahia'
+
+//name ColName
+document.getElementById('colName').innerHTML = 'Alexander Pire Almeida'
+
+//name CPF
+document.getElementById('cpf').innerHTML = '326.754.745-84'
+
+//name Hora/Data
+document.getElementById('hourDate').innerHTML = '09/10/2023 - 08:22h'
+
+
+
+
+
+
